32da456a7d1447b76d66b77f65186f5d20fc64a7
Manya <manya111203@gmail.com>
2025-11-27 02:27:28 +0530

    update: moved all the pages(folders in app folder) to the route group (website) folder in app folder for admin isolation

--- app/(website)/layout.tsx
+++ app/(website)/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "../globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import BottomNav from "@/components/BottomNav";
