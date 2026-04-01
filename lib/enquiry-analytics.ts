97a1a0d9612657bf5845c5a1d780d6ec68f16304
krishna-dtu <kj20130284870@gmail.com>
2026-04-02 01:40:04 +0530

    fixes

--- lib/enquiry-analytics.ts
+++ lib/enquiry-analytics.ts
@@ -1,4 +1,4 @@
-import { Enquiry } from "@prisma/client";
+import { Enquiry } from "@/lib/generated/prisma";
 import { startOfDay, subDays, format, isWithinInterval, startOfWeek } from "date-fns";
 
 export interface ServiceCount {
